1b2428b2829aefd7541b969f6d5a8d7af84fd834
kozaaaaczx <kozaaaaczx@users.noreply.github.com>
2026-05-13 18:37:57 +0200

    update base path for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Zamień 'nazwa-repozytorium' na rzeczywistą nazwę Twojego folderu/repo na GitHubie
-  base: './', 
+  base: '/KGL/', 
 })
